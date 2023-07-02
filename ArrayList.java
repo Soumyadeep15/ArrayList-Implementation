b4baeed1c2cdfcf0d71e19725042df7f7a709d7c
Soumyadeep15 <108778550+Soumyadeep15@users.noreply.github.com>
2023-07-02 20:37:14 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class ArrayList{
+    private Object[] a;
+    private int p;
+    ArrayList(){
+        a= new Object[3];
+        p=0;
+    }
+
+    public void add(Object e){
+        if(p>=a.length) increase();
+        a[p]=e;
+        p++;
+    }
+
+    public void add(int index,Object e) {
+        if(index<=-1 || index>=a.length){
+            throw new IndexOutOfBoundsException();
+        }
+        if(p>=a.length) increase();
+        for(int i=index;i<=size();i++){
+            a[i]=a[i+1];
+        }
+        a[index]=e;
+        p++;
+    }
+
+    private void increase(){
+        Object[] temp=new Object[a.length+3];
+        System.arraycopy(a, 0, temp, 0, a.length);
+        a=temp;
+    }
+
+    public Object get(int index){
+        if(index<=-1 || index>=a.length){
+            throw new IndexOutOfBoundsException();
+        }
+        return a[index];
+    }
+    public void remove(int index){
+        if(index<=-1 || index>=a.length){
+            throw new IndexOutOfBoundsException();
+        }
+        for(int i=index;i<=size();i++){
+            a[i]=a[i+1];
+        }
+        a[p]=null;
+        p--;
+    }
+
+    public int size(){
+        return p;
+    }
+
+    public void printItems(){
+        System.out.println(Arrays.toString(a));
+    }
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+    public static void main(String[] args) {
+        ArrayList a=new ArrayList();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        
+        a.printItems();
+
+    }
+}
